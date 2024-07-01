d2845222081faa3d67a976b6f2c3bfbd1226fee7
Mahesh C <mahesh.fisat@gmail.com>
2024-07-01 08:24:47 +0530

    pointer2

---
+++ ex3-pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void modifyValue(int *ptr) {
+    *ptr = 20;
+}
+
+
+int main() {
+    // Basic pointer declaration and assignment
+    int num = 10;
+    int *ptr = &num;
+    modifyValue(ptr);
+    printf("Value after function call %d\n",*ptr);
+    return 0;
+}
+
